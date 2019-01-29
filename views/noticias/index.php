3a491e138de9a4009058b1dc57a301c0ea987772
Jonatan Cerezuela López <jcerezuelalopez@gmail.com>
2019-01-29 18:12:12 +0100

    casi listo

---
+++ views/noticias/index.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\NoticiasSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Noticias';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="noticias-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Noticias', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'titulo',
+            'descripcion:ntext',
+            'url:url',
+            'usuario.nombre:text:Usuario',
+            'categoria.categoria:text:Categoria',
+            'created_at',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
