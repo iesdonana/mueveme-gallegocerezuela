c91a03b540c4a7f997b0e8864b0b4bfab2da4002
Jonatan Cerezuela López <jcerezuelalopez@gmail.com>
2019-01-31 20:50:01 +0100

    Boton reubicado

--- views/noticias/_detalle.php
+++ views/noticias/_detalle.php
@@ -8,10 +8,10 @@ function saca_dominio($url){
 }
 ?>
 <div class="row">
-    <div class="col-md-2">
+    <div class="col-md-2" style='padding:70px'>
         <button type="submit" name="button" class='btn btn-info'>Movévelo</button>
     </div>
-    <div class="col-md-9 col-md-offset-2">
+    <div class="col-md-9">
         <h3><?= Html::a($model->titulo, $model->url) ?></h3>
         <p>por <strong><?= $model->usuario->nombre ?></strong> a <strong><?= DomainExtractor::fromUrl($model->url) ?></strong>  publicado <?= Yii::$app->formatter->asDatetime($model->created_at)  ?></p>
         <p><?= $model->descripcion ?></p>
